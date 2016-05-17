529f2164da5314f02f0f418c9459a9030de6c72b
Volodymyr Gamula <v.gamula@gmail.com>
2016-05-17 22:21:39 +0300

    Need to think how to store questionId and right answer

--- index.js
+++ index.js
@@ -3,23 +3,75 @@ const co = require('co');
 
 const config = require('./config');
 
-console.log(config);
 
+let ph = null;
 
-let phantomInstance = null;
+const sleep = (time) => new Promise(res => {
+    setTimeout(() => {
+        res(true);
+    }, time)
+});
+
+const waitFor = co.wrap(function *(testFunc, timeOutMillis) {
+    return new Promise((res, rej) => {
+        const start = new Date().getTime();
+        const maxtimeOutMillis = timeOutMillis || 3000;
+        const condition = false;
+        while ((new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+            condition = yield* testFunc();
+            if (!condition) {
+                yield sleep(100);
+            } else {
+                break;
+            }
+        }
+        if (condition) {
+            res();
+        } else {
+            rej();
+        }
+    };
+});
+
 
 const main = co.wrap(function *() {
-    phantomInstance = yield phantom.create();
+    ph = yield phantom.create();
+
+    const page = yield ph.createPage();
+    yield page.open(config.host);
+    yield page.evaluate(function() {
+        document.getElementById('login_username').value = 'vgamula';
+        document.getElementById('login_password').value = 'q1w2e3';
+        document.getElementById('login').submit();
+    });
+    yield sleep(1000);
+    yield page.open('https://msn.khnu.km.ua/mod/quiz/view.php?id=198028');
+    yield sleep(1000);
+    yield page.evaluate(function() {
+        document.forms[0].submit();
+    });
+    yield sleep(1000);
+
+    const [qidVal, answId] = yield page.evaluate(function() {
+        var questions = document.getElementsByClassName('que');
+        var firstQuestionElem = questions[0];
+        var qidVal = firstQuestionElem.getElementsByClassName('questionflagpostdata')[0].value;
+        qidVal = qidVal.slice(qidVal.indexOf('qid=') + 4);
+        qidVal = qidVal.slice(0, qidVal.indexOf('&'));
+        return [qidVal, 0];
+    });
+    const content = yield page.property('content');
+    console.log(content);
 })
 
 main().then(() => {
-    phantomInstance.exit();
+    ph.exit();
 }).catch(() => {
-    phantomInstance.exit();
+    ph.exit();
 });;
 
 process.on('SIGINT', () => {
-    phantomInstance.exit();
+    ph.exit();
     console.log('Bye :)');
     process.exit();
 });
